0a4c193fa74fc1ca4197e9bc75bafce0fd5343ba
Sam Davis <sam.davis@adrenalinmedia.com.au>
2019-09-10 09:30:58 +1000

    add the ability to set custom status bar display

--- README.md
+++ README.md
@@ -7,14 +7,33 @@ This extension displays the current state of the `editor.formatOnSave` setting i
 -   Shows the current status of the `editor.formatOnSave` status in the status bar
 -   Allows you to quickly toggle this setting on/off by clicking the item in the status bar
 -   Exposes the command `statusbar-format-on-save.toggleFormatOnSave` for easy keybinding or command pallate usage
+-   Allows customisation of the display in the status bar (with a custom format string)
 
 ![screenshot](https://i.imgur.com/PbMSJ2q.png)
+
+## Examples
+
+### Format String Customisation
+
+Using the following settings will result in the status bar display showing as `FoS: 👍` or `FoS: 👎`
+
+```json
+{
+    "statusbar-format-on-save.formatString": "FoS: {{status}}",
+    "statusbar-format-on-save.displayWhenTrue": "👍",
+    "statusbar-format-on-save.displayWhenFalse": "👎"
+}
+```
 
 ## Issues
 
 If you find an issue with this extension or would like to request new functionality, please let me know [here on GitHub](https://github.com/treetrum/statusbar-format-on-save).
 
 ## Release Notes
+
+### 1.1.0
+
+Allow for customisation of the display in the status bar (with a custom format string)
 
 ### 1.0.0
 
--- extension.js
+++ extension.js
@@ -31,14 +31,56 @@ function activate(context) {
 }
 exports.activate = activate;
 
+/**
+ * Constants to use when accessing the plugin configuration object
+ */
+const settings = {
+    formatString: "formatString",
+    displayWhenTrue: "displayWhenTrue",
+    displayWhenFalse: "displayWhenFalse",
+    statusTokenVar: "{{status}}"
+};
+
+/**
+ * Gets the configuration object for the editor settings
+ */
 const getEditorConfig = () => {
     return vscode.workspace.getConfiguration("editor", null);
 };
 
-const updateStatusBarItem = () => {
+/**
+ * Gets the configuration object for this plugin
+ */
+const getPluginConfig = () => {
+    return vscode.workspace.getConfiguration("statusbar-format-on-save", null);
+};
+
+/**
+ * Get's either the displayWhenTrue or displayWhenFalse value
+ */
+const getValueDisplay = () => {
     const config = getEditorConfig();
     const enabled = config.get("formatOnSave");
-    statusBarItem.text = `Format on save: ${enabled ? "Enabled" : "Disabled"}`;
+    return getPluginConfig().get(
+        enabled ? settings.displayWhenTrue : settings.displayWhenFalse
+    );
+};
+
+/**
+ * Gets the format string from user preferences and merges in the valueDisplay values.
+ * The return value from this function can directly be put into the status bar
+ */
+const getMergedFormatString = () => {
+    return getPluginConfig()
+        .get(settings.formatString)
+        .replace(settings.statusTokenVar, getValueDisplay);
+};
+
+/**
+ * Main worker function that does the job of updating the status bar item
+ */
+const updateStatusBarItem = () => {
+    statusBarItem.text = getMergedFormatString();
     if (vscode.window.activeTextEditor) {
         statusBarItem.show();
     } else {
@@ -46,6 +88,9 @@ const updateStatusBarItem = () => {
     }
 };
 
+/**
+ * Toggles the current status of the formatOnSave value
+ */
 const toggleFormatOnSave = () => {
     const config = getEditorConfig();
     const enabled = config.get("formatOnSave");
